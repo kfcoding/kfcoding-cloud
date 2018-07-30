429f956babed7506ed34b8730b0e5c7536b7fc47
mapleB <515186469@qq.com>
2018-07-30 15:38:32 +0800

    环境搭建

---
+++ kfcoding-zipkin/src/main/java/com/cuiyun/kfcoding/zipkin/ZipkinMsApplication.java
@@ -0,0 +1,14 @@
+package com.cuiyun.kfcoding.zipkin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import zipkin.server.EnableZipkinServer;
+
+@EnableZipkinServer
+@SpringBootApplication
+public class ZipkinMsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ZipkinMsApplication.class, args);
+    }
+}
