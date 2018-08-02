f1830458dbe58b1b509a1ae88417ff8480d65541
mapleB <515186469@qq.com>
2018-08-02 14:07:54 +0800

    环境搭建

---
+++ kfcoding-framework/framework-common/src/main/java/com/cuiyun/kfcoding/framework/common/base/model/BaseModel.java
@@ -0,0 +1,10 @@
+package com.cuiyun.kfcoding.framework.common.base.model;
+
+/**
+ * @program: kfcoding-cloud
+ * @description: model基础类
+ * @author: maple
+ * @create: 2018-08-02 14:03
+ **/
+public class BaseModel {
+}
